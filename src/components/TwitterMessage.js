9d3a2a155f3c6e88d9093a1df8afd17f9bda58c9
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-14 09:28:55 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor(maxChars ) {
-    super();
+  constructor(maxChars) {
+    super(maxChars);
 
     this.state = {};
   }
